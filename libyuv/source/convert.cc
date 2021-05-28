f21b0e01f9925f74e0234b3bb41c06959de7d928
Tran Binh Trong <binhtrong.tr@samsung.com>
2021-05-28 11:03:24 +0700

    add code

--- libyuv/source/convert.cc
+++ libyuv/source/convert.cc
@@ -15,7 +15,6 @@
 #include "libyuv/planar_functions.h"
 #include "libyuv/rotate.h"
 #include "libyuv/row.h"
-#include "vector"
 #include "libyuv/scale.h"     // For ScalePlane()
 #include "libyuv/scale_uv.h"  // For UVScale()
 
@@ -3141,51 +3140,6 @@ int Android420ToI420(const uint8_t* src_y,
     dst_v += dst_stride_v;
   }
   return 0;
-}
-
-
-void NV12ToI420Scaler::NV12ToI420Scale(const uint8_t* src_y,
-                                       int src_stride_y,
-                                       const uint8_t* src_uv,
-                                       int src_stride_uv,
-                                       int src_width,
-                                       int src_height,
-                                       uint8_t* dst_y,
-                                       int dst_stride_y,
-                                       uint8_t* dst_u,
-                                       int dst_stride_u,
-                                       uint8_t* dst_v,
-                                       int dst_stride_v,
-                                       int dst_width,
-                                       int dst_height) {
-  if (src_width == dst_width && src_height == dst_height) {
-    // No scaling.
-    tmp_uv_planes_.clear();
-    tmp_uv_planes_.shrink_to_fit();
-    libyuv::NV12ToI420(src_y, src_stride_y, src_uv, src_stride_uv, dst_y,
-                       dst_stride_y, dst_u, dst_stride_u, dst_v, dst_stride_v,
-                       src_width, src_height);
-    return;
-  }
-
-  // Scaling.
-  // Allocate temporary memory for spitting UV planes.
-  const int src_uv_width = (src_width + 1) / 2;
-  const int src_uv_height = (src_height + 1) / 2;
-  tmp_uv_planes_.resize(src_uv_width * src_uv_height * 2);
-  tmp_uv_planes_.shrink_to_fit();
-
-  // Split source UV plane into separate U and V plane using the temporary data.
-  uint8_t* const src_u = tmp_uv_planes_.data();
-  uint8_t* const src_v = tmp_uv_planes_.data() + src_uv_width * src_uv_height;
-  libyuv::SplitUVPlane(src_uv, src_stride_uv, src_u, src_uv_width, src_v,
-                       src_uv_width, src_uv_width, src_uv_height);
-
-  // Scale the planes into the destination.
-  libyuv::I420Scale(src_y, src_stride_y, src_u, src_uv_width, src_v,
-                    src_uv_width, src_width, src_height, dst_y, dst_stride_y,
-                    dst_u, dst_stride_u, dst_v, dst_stride_v, dst_width,
-                    dst_height, libyuv::kFilterBox);
 }
 
 #ifdef __cplusplus
